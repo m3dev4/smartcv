80b1e5918f71ced1314a8966391ef661f6bb3d26
m3dev4 <m3dev4@gmail.com>
2025-06-26 18:46:36 +0000

    add render cli

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(bodyParser.json({limit: '50mb'}));
